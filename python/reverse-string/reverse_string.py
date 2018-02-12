812c56b4735f4d0c4ee828df9f44fe1632b62062
garritfra <garritfranke@gmail.com>
2018-02-12 22:12:57 +0100

    finish python reverse string

--- python/reverse-string/reverse_string.py
+++ python/reverse-string/reverse_string.py
@@ -1,9 +1,2 @@
 def reverse(string):
-    reversedString = ""
-
-    iteration = 0
-
-    for char in string:
-        reversedString[len(string) - iteration] += string[len(string) - char.index]
-        iteration += 1
-    return reversedString
+    return string[::-1] #See "Extended Splice"
